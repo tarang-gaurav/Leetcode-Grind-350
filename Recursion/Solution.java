8a6b67a8a46cea60d53718fbc2e823421335e952
LeoCodes <tarang.terrific@gmail.com>
2023-03-21 23:23:36 +0530

    "Printing name 5 times using recursion"

--- Recursion/Solution.java
+++ Recursion/Solution.java
@@ -1,11 +1,22 @@
 class Solution{
 
 
+    void printRecursion(int x){
+        if(x==0)
+        {
+            return;
+        }else{
+            System.out.println("Code");
+            printRecursion(x-1);
+        }
+
+        
+    }
 
     public static void main(String[] args){
-
+        Solution s=new Solution();
         System.out.println("Welcome!!!");
-
+        s.printRecursion(5);
 
     }
 
