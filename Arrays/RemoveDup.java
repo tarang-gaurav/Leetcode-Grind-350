fbc384ad5676f4a132743dc1ee4fae041a1f3e98
LeoCodes <tarang.terrific@gmail.com>
2023-03-26 14:15:19 +0530

    "Remove Dup from sorted array without extra space"

--- Arrays/RemoveDup.java
+++ Arrays/RemoveDup.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(i<nums.length-1 && nums[i]==nums[i+1]){
+                continue; 
+                       }
+            ///updating sorted array in-place
+            nums[count]=nums[i];
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        Solution s=new Solution();
+        int[] arr={0,1,1,2,2,2,2,3};
+        s.removeDuplicates(arr);
+    }
+
+}
 
