624a864d640a26b5ebf6a186187603826b2b7d75
LeoCodes <tarang.terrific@gmail.com>
2023-04-02 02:02:37 +0530

    "Power of N,R using recursion in modulo"

--- Recursion/SolutionPowerNum.java
+++ Recursion/SolutionPowerNum.java
@@ -1,12 +1,34 @@
 class SolutionPowerNum{
 
+    long power(int N,int R)
+    {
+        //Your code here
+       
+        long modulo=1000000000+7;
+        if(R==0)
+            return 1;
+        else if (R==1)
+            return N;
+        else{
+           long temp=power(N,R/2);
+            temp=(temp*temp)%modulo;
+            if(R%2!=0)
+                return (temp*N)%modulo;
+            return temp;
 
 
+        }
+     
+        
+    }
+/**
+ * Solution: Power after reversing a digit 
+ * 
+ * @param args
+ */
     public static void main(String[] args){
-
-        System.out.println("SolutionPowerNum!!!");
-        System.out.println("test2 !!!");
-
+        SolutionPowerNum s=new SolutionPowerNum();
+        System.out.println(s.power(12, 21));
     }
 
 }
