1e9df74639b0933f995e1cd61bdaf2a8564e48c1
LeoCodes <tarang.terrific@gmail.com>
2023-08-27 22:42:36 +0530

    Test 1

---
+++ Tree/VerticalLevelT.java
@@ -0,0 +1,9 @@
+public class VerticalLevelT {
+    
+    public int T(){
+        System.out.println("dk");
+    }
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
