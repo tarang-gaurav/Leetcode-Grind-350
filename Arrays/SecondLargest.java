b27708c1c36c03a5c315db871a0208961c64099c
LeoCodes <tarang.terrific@gmail.com>
2023-10-03 22:54:50 +0530

    Second Largest Elem

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -1,10 +1,10 @@
 public class SecondLargest {
-    
-    int print2largest(int arr[], int n) {
+   
+    static int print2largest(int arr[], int n) {
         // code here     
         int max=arr[0];
         int secondMax=-1;
-        
+        // 3,4,3,1,7,9
         for(int i=1;i<n;i++){
             if(max<arr[i]){
                 secondMax=max;
@@ -17,6 +17,11 @@ public class SecondLargest {
         }
     
         return secondMax;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={3,4,3,1,2,5,7,9};
+        System.out.println(print2largest(arr, 8));
     }
 
     
