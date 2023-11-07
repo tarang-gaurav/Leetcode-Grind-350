b74477a2b8aeceac0f8713cbde82a3e7e1b966b8
LeoCodes <tarang.terrific@gmail.com>
2023-11-07 23:50:41 +0530

    Kaden algo

--- Arrays/KadenAlgo.java
+++ Arrays/KadenAlgo.java
@@ -10,16 +10,18 @@ class Kaden{
         for(int i=0;i<n;i++){
             sum+=arr[i];
             max=Math.max(max, sum);
-
+            if(sum<=0) sum=0;
             
         }
 
 
-        return 0;
+        return max;
     }
     public static void main(String[] args) {
         int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Kaden k=new Kaden();
          int n =arr.length;
-         int maxSum = maxSubarraySum(arr, n);
+         int maxSum = k.maxSubarraySum(arr, n);
+         System.out.println(maxSum);
     }
 }
