5245e74c4305c79503775bb56bf54b29c6b0fb84
LeoCodes <tarang.terrific@gmail.com>
2023-12-03 13:00:11 +0530

    Find steps in climbing stairs

---
+++ DP/ClimbStairs.java
@@ -0,0 +1,17 @@
+package DP;
+
+public class ClimbStairs {
+    
+    private static int findStairs(int n){
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+    return dp[n];
+    }
+    public static void main(String[] args) {
+        System.out.println(findStairs(5));
+    }
+}
