e92c3266945f39268c9f6d0afb9b767b34af3455
tarang-gaurav <tarang.terrific@gmail.com>
2025-06-13 02:01:04 +0530

    Binary search - iterative

--- Arrays/BinarySearch/BinarySearch.java
+++ Arrays/BinarySearch/BinarySearch.java
@@ -1,1 +1,29 @@
+package BinarySearch;
 
+public class BinarySearch{
+
+    private static int binarySearch(int[] arr , int x){
+
+        int low = 0;
+        int high = arr.length -1 ;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]<x)
+                low = mid+1;
+            else if(arr[mid]>x)
+                high = mid-1;
+            else
+                return mid;
+        }
+        return -1;
+    
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hi binary!!!");
+        System.out.println("Index at : "+binarySearch
+        (new int[]{1,2,5,67,78,89,90,2234,35345,5464464}, 93240));
+    }
+}
